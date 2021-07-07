65f563a28506b4dffe00bd17a6b045103f603493
adasoraninda <adasoraninda@gmail.com>
2021-07-07 21:26:02 +0700

    Get image from network, but app will crash beause network calls

--- app/src/main/java/com/adasoraninda/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/com/adasoraninda/kotlincoroutines/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.adasoraninda.kotlincoroutines
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var image: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        image = findViewById(R.id.image)
+
+        val imageUrl = URL("https://wallpaperplay.com/walls/full/1/c/7/38027.jpg")
+
+        val connection = imageUrl.openConnection() as HttpURLConnection
+        connection.doInput = true
+        connection.connect()
+
+        val inputStream = connection.inputStream
+        val bitmap = BitmapFactory.decodeStream(inputStream)
+
+        image.setImageBitmap(bitmap)
     }
 }
